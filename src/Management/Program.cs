06a92f4091aa10792fc9a24084f07776bd8958bf
Saffian-Asghar <baigsaffian@gmail.com>
2024-05-17 11:53:58 +0200

    PostGres

--- src/Management/Program.cs
+++ src/Management/Program.cs
@@ -4,11 +4,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 builder.Services.AddDbContext<StudentManagementDbContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,5 +20,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+try 
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 app.Run();
