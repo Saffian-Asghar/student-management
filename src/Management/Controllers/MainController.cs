d046afea0ecd202540afcc97d72be0b3fda94970
Saffian-Asghar <baigsaffian@gmail.com>
2024-05-17 11:14:31 +0200

    init

---
+++ src/Management/Controllers/MainController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Management.Controllers
+{
+    public class MainController : Controller
+    {
+        // Define your controller actions here
+    }
+}
