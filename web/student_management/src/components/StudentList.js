f95b00c1023e61ad18e72e50df225d4c451c33c6
bmarid <mary.baj@yandex.ru>
2024-05-24 10:12:42 +0200

    add style, fix adding empty user

--- web/student_management/src/components/StudentList.js
+++ web/student_management/src/components/StudentList.js
@@ -13,7 +13,7 @@ const StudentList = (props) => {
     
     const togglePopup = () => {
       setIsPopupOpen(!isPopupOpen);
-
+      if (username) {
       axios.post('http://localhost:5010/api/students', {
           "name": username,
           "email": "",
@@ -27,18 +27,18 @@ const StudentList = (props) => {
         .catch(function (error) {
           console.log(error);
         });
+      }
     };
   
     const handleUsernameChange = (event) => {
       setUsername(event.target.value);
     };
-  
     
     return (
         <table className='table'>
             <thead className="thead-light">
             <tr>
-              <th scope="col">Studdddent ID</th>
+              <th scope="col">Student ID</th>
               <th scope="col">Name</th>
               <th scope="col">Courses</th>
             </tr>
@@ -48,19 +48,31 @@ const StudentList = (props) => {
                 <Student id={student.id} key={student.id} name={student.name} />
             ))}
             </tbody>
-            <button onClick={togglePopup}>Add Username</button>
-            
+            <button type="submit" class="btn btn-primary" onClick={togglePopup}>Add Username</button>
             {isPopupOpen && (
             <div className="popup">
               <div className="popup-inner">
-                <h2>Enter Username</h2>
-                <input type="text" value={username} onChange={handleUsernameChange} placeholder="Username"/>
+                <form>
+                <h3>Enter a new student</h3>
+                <div class="mb-4 input-group">
+                  <span class="input-group-text">
+                    <i class="bi bi-person"></i>
+                  </span> 
+                <input type="text" class="form-control" id="username" value={username} onChange={handleUsernameChange} placeholder="Username"/>
+                </div>
                 {/* <input type="text" value={userEmail} placeholder="Email"/>
                 <input type="text" value={DateOfBirth} placeholder="DateOfBirth"/>
                 <input type="text" value={Address} placeholder="Address"/>
                 <input type="text" value={PhoneNumber} placeholder="PhoneNumber"/>  */}
-                <button onClick={togglePopup}>AddUser</button>
-                <button onClick={togglePopup}>Close</button>
+                <select class="form-select" aria-label='Math-Default select example'>
+                  <option selected>Open this select menu</option>
+                  <option value="1">o_Math</option>
+                  <option value="2">o_Art</option>
+                </select>
+
+                <button type="submit" class="btn btn-primary" onClick={togglePopup}>AddUser</button>
+                <button type="submit" class="btn btn-primary" onClick={togglePopup}>Close</button>
+                </form>
               </div>
             </div>
           )}
